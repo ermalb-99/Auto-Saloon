2b8bbaab73b6354df1d6fd07a3cfcf0baae1e28b
ermalb-99 <ermal_beqiri@outlook.com>
2024-01-09 13:33:47 +0100

    desploy

---
+++ main.js
@@ -0,0 +1,30 @@
+document.querySelector('.SUB').addEventListener('click', function () {
+     document.querySelector('.SUB').innerHTML = 'Subscribed!';
+ });
+
+ /* When the user clicks on the button, 
+toggle between hiding and showing the dropdown content */
+function myFunction() {
+     document.getElementById("myDropdown").classList.toggle("show");
+   }
+   
+   // Close the dropdown if the user clicks outside of it
+   window.onclick = function(event) {
+     if (!event.target.matches('.dropbtn')) {
+       var dropdowns = document.getElementsByClassName("dropdown-content");
+       var i;
+       for (i = 0; i < dropdowns.length; i++) {
+         var openDropdown = dropdowns[i];
+         if (openDropdown.classList.contains('show')) {
+           openDropdown.classList.remove('show');
+         }
+       }
+     }
+   }
+
+ 
+   document.querySelector('.download').addEventListener('click', function () {
+    document.querySelector('.download').innerHTML = 'Download Started!';
+});
+
+
